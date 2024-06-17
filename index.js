63a1d8bdebcecd2207c2c1e5e2df641af68f0e40
Zaitsev Mykola <zaitsev.nikolay11@gmail.com>
2024-06-17 17:36:17 +0300

    task2

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function task1(num) {
 const addNum = task1(5);
 console.log(addNum(3));
 
+function task2(startValue, step) {
+    let coynt = startValue;
+  function increment() {
+    return (coynt += step);
+  }
+  function decrement() {
+    return (coynt -= step);
+  }
+  function reset() {
+    return (coynt = startValue);
+  }
 
+  return [increment,decrement,reset];
+}
+
+const [increment,decrement,reset] = task2(2,3);
+
+
+console.log(increment()); 
+console.log(increment()); 
+console.log(decrement()); 
+console.log(reset());
